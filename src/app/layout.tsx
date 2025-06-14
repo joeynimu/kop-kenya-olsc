0594bfbe5b8cb454ee8a92648e5559b03704832c
Joe Ng'ethe <joeynimu@gmail.com>
2025-06-14 10:13:37 +0300

    WIP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,8 +13,8 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "KOP Kenya OLS",
-  description: "Official KOP Kenya OLS",
+  title: "KOP Kenya OLSC",
+  description: "Official KOP Kenya OLSC",
 };
 
 export default function RootLayout({
